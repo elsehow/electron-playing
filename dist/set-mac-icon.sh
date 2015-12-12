6dcefde9e66af1f7c5691c5a7aeda5f9275563d8
elsehow <yes@cosmopol.is>
2015-12-12 13:05:22 -0800

    added shell script for setting mac icon

---
+++ dist/set-mac-icon.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Copying icons for mac"
+
+cp dist/resources/Info.plist dist/SpectralCharmer-darwin-x64/SpectralCharmer.app/Contents/
+
+cp dist/resources/dragon.icns dist/SpectralCharmer-darwin-x64/SpectralCharmer.app/Contents/Resources/dragon.icns
